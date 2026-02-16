60e79c1f50f08e2cc017415cfa9619908af51b6d
nodeloc <james@nodeloc.com>
2026-02-16 10:47:43 +0800

    fix bug

--- web/src/views/AdminCoupons.vue
+++ web/src/views/AdminCoupons.vue
@@ -368,10 +368,13 @@ const formatValue = (coupon) => {
 }
 
 const formatDate = (dateString) => {
-  return new Date(dateString).toLocaleDateString('en-US', {
+  return new Date(dateString).toLocaleString('en-US', {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
   })
 }
 </script>
