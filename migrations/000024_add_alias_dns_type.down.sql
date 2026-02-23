c4c58e62e8f17610a534e869bd882c251f3f325a
nodeloc <james@nodeloc.com>
2026-02-23 21:46:54 +0800

    add alias and whois

---
+++ migrations/000024_add_alias_dns_type.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE dns_records DROP CONSTRAINT IF EXISTS dns_records_type_check;
+ALTER TABLE dns_records ADD CONSTRAINT dns_records_type_check CHECK (type IN ('A', 'AAAA', 'CNAME', 'MX', 'TXT', 'NS', 'SRV', 'CAA'));
